205f5a82479977f455e008e38fad989db4d289f9
Mirajiik <mirajiik@yandex.ru>
2023-09-18 16:27:50 +0500

    Create project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Messanger.ViewModels.Base;
+
+namespace Messanger.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title;
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
